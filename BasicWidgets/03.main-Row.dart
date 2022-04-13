fb561d31dd17045b1b67627209d46fef9deed1d3
diffth <diffth@nate.com>
2022-04-13 18:40:21 +0900

    Update 03.main-Row.dart

--- BasicWidgets/03.main-Row.dart
+++ BasicWidgets/03.main-Row.dart
@@ -1,1 +1,69 @@
-1
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Row(
+        children: const [
+          Icon(
+            Icons.camera_alt,
+            color: Colors.red,
+            size: 35.0,
+          ),
+          Icon(
+            Icons.cancel,
+            color: Colors.orange,
+            size: 35.0,
+          ),
+          Icon(
+            Icons.audiotrack,
+            color: Colors.green,
+            size: 30.0,
+          ),
+          Icon(
+            Icons.beach_access,
+            color: Colors.blue,
+            size: 36.0,
+          ),
+          Icon(
+            Icons.add_a_photo_rounded,
+            color: Colors.purple,
+            size: 36.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
