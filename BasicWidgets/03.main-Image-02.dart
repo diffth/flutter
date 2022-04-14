7e2d0758da26ae596724abbd50f305456edcc3e4
diffth <diffth@nate.com>
2022-04-14 15:25:09 +0900

    Update 03.main-Image-02.dart

--- BasicWidgets/03.main-Image-02.dart
+++ BasicWidgets/03.main-Image-02.dart
@@ -1,1 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      // body: Image.asset('assets/images/900.png',
+      //   // 사이즈 지정하면 자동설정
+      //   fit: BoxFit.fill,
+      //   alignment: Alignment.topLeft,
+      //   // 원래 사이즈 비율로
+      //   width: 150.0,
+      // ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset('assets/images/900.png',
+            fit: BoxFit.fill,
+            alignment: Alignment.topLeft,
+            width: 150.0,
+          ),
+          const SizedBox(height: 5),
+          Image.asset('assets/images/900.png',
+            fit:BoxFit.cover,
+            alignment: Alignment.centerLeft,
+            width: 150.0,
+            height: 100.0,
+          ),
+          const SizedBox(height: 5,),
+          Image.asset('assets/images/900.png',
+            fit: BoxFit.fill,
+            alignment: Alignment.centerLeft,
+            width: 150.0,
+            height: 100.0,
+          ),
+          const SizedBox(height: 5,),
+          Image.asset('assets/images/900.png',
+            fit: BoxFit.contain,
+            alignment: Alignment.topLeft,
+            width: 150.0,
+            height: 100.0,
+          ),
+          // const SizedBox(height: 5,),
+          // Image.asset('assets/images/900.png',
+          //   alignment: Alignment.topLeft,
+          //   width: 150.0,
+          //   height: 100.0,
+          // ),
+          const SizedBox(height: 5,),
+          Image.asset('assets/images/900.png',
+            fit: BoxFit.none,
+            alignment: Alignment.topLeft,
+            width: 320.0,
+            height: 80.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
 
