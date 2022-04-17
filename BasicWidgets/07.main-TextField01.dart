315a9c5163858cdb41107b5ed19dcae09c5c3974
diffth <diffth@nate.com>
2022-04-18 07:24:14 +0900

    Create 07.main-TextField01.dart

---
+++ BasicWidgets/07.main-TextField01.dart
@@ -0,0 +1,78 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _myText = '세종대왕';
+  final ctlMyText1 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              _myText,
+              style: const TextStyle(fontSize: 30.0),
+            ),
+            SizedBox(
+              width: 280,
+              child: TextField(
+                controller: ctlMyText1,
+              ),
+            ),
+            ElevatedButton(
+              child: const Text('ElevatedButton', style: TextStyle(fontSize: 24),),
+              onPressed: () {
+                setState(() {
+                  _myText = ctlMyText1.text;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _myText = '세종대왕';
+          });
+        },
+        child: const Icon(Icons.send),
+      ),
+    );
+  }
+}
