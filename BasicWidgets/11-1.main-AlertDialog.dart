8a5740d7a137314574f2b5bbe00a27e117ae3078
diffth <diffth@nate.com>
2022-04-19 14:46:37 +0900

    Update 11-1.main-AlertDialog.dart

--- BasicWidgets/11-1.main-AlertDialog.dart
+++ BasicWidgets/11-1.main-AlertDialog.dart
@@ -1,1 +1,101 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _sPersonName = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text('Show AlertDialog', style: TextStyle(fontSize: 24, color: Colors.white),),
+              onPressed: () => _showAlertDialog(context, 'hello~'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future _showAlertDialog(BuildContext context, String message) async {
+    await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context){
+        return Theme(
+          data: ThemeData(dialogBackgroundColor: Colors.orange),
+          child: AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            title: Text('AlertDialog Example'),
+            content: Container(
+              height: 90,
+              child: Column(
+                children: [
+                  Text(message),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Name', hintText: '세종대왕'),
+                    onChanged: (value){
+                      _sPersonName = value;
+                    },
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              ElevatedButton(
+                child: const Text('OK'),
+                onPressed: () {
+                  Navigator.pop(context, 'OK');
+                  print('OK - $_sPersonName');
+                },
+              ),
+              ElevatedButton(
+                child: const Text('Cancel'),
+                onPressed: () {
+                  Navigator.pop(context, 'Cancel');
+                  print('Cancel');
+                },
+              ),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
 
