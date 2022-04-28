c5a212a3c77dca27a052ecf3837d1e0cf403f363
diffth <diffth@nate.com>
2022-04-28 13:19:10 +0900

    Update 16-2.main-GridView2.dart

--- BasicWidgets/16-2.main-GridView2.dart
+++ BasicWidgets/16-2.main-GridView2.dart
@@ -1,1 +1,76 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                child: GridView.count(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.5,
+                  children: List.generate(50, (index){
+                    return Container(
+                      child: Card(
+                        color: Colors.blue,
+                      ),
+                    );
+                  }),
+                ),
+              ),
+              Container(
+                height: 200,
+                child: GridView.count(
+                  scrollDirection: Axis.horizontal,
+                  crossAxisCount: 2,
+                  children: List.generate(50, (index){
+                    return Container(
+                      child: Card(
+                        color: Colors.amber,
+                      ),
+                    );
+                  }),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
