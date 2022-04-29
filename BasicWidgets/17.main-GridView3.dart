e59c26a5ec8bfe92605dfb4d3e0028a907c3d82b
diffth <diffth@nate.com>
2022-04-29 13:56:31 +0900

    Update 17.main-GridView3.dart

--- BasicWidgets/17.main-GridView3.dart
+++ BasicWidgets/17.main-GridView3.dart
@@ -1,1 +1,145 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter GridView'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late List<Person> persons;
+
+  @override
+  void initState() {
+    super.initState();
+
+    persons = [];
+    for(int i=0; i<15; i++){
+      persons.add(Person(i+21, '이순신$i', true));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        height: 400.0,
+        child: GridView.count(
+          padding: const EdgeInsets.all(10),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 3,
+          childAspectRatio: 0.7,
+          children: getGridPage(),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> getGridPage() {
+    List<Widget> grid = [];
+    for(int i=0; i<persons.length; i++){
+      Widget myWidget = Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(2),
+        color: Colors.blue[100],
+        child: PersonTile(persons[i], i),
+      );
+      grid.add(myWidget);
+    }
+    return grid;
+  }
+}
+
+class PersonTile extends StatelessWidget{
+  final Person _person;
+  final int index;
+
+  PersonTile(this._person, this.index);
+
+  @override
+  Widget build(BuildContext context){
+    return Card(
+      color: Colors.amber[100],
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: Colors.black, width: 1),
+        borderRadius: BorderRadius.circular(0),
+      ),
+      child: InkWell(
+        splashColor: Colors.blue,
+        onTap: () {
+          print('$index Tile Click~!');
+        },
+        child: SizedBox(
+          width: 140,
+          height: 200,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 100,
+                alignment: Alignment.center,
+                color: Colors.blue[50],
+                child: Column(
+                  children: [
+                    Text(
+                      _person.name,
+                      style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "'${_person.age}세'",
+                      style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.normal),
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                child: const Text('ElevatedButton', style: TextStyle(fontSize: 14),),
+                onPressed: ()=> _onClick(index),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onClick(int index) {
+    print('$index click~!');
+  }
+}
+
+class Person {
+  int age;
+  String name;
+  bool isLeftHand;
+
+  Person(this.age, this.name, this.isLeftHand);
+}
 
