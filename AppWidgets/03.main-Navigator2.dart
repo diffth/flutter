0696997cadda4deb8cd322de226003db9cea657a
diffth <diffth@nate.com>
2022-05-06 13:52:16 +0900

    Create 03.main-Navigator2.dart

---
+++ AppWidgets/03.main-Navigator2.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Navigator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Page1(),
+    );
+  }
+}
+
+class Page1 extends StatefulWidget {
+  const Page1({ Key? key }) : super(key: key);
+
+  @override
+  State<Page1> createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
+  @override
+  Widget build(BuildContext context) {
+    print('Page1');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page 1'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text('2페이지 교체', style: TextStyle(fontSize: 24),),
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, anim1, anim2) => const Page2(),
+                    transitionDuration: const Duration(seconds: 0),
+                  ),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: const Text('3페이지로 교체', style: TextStyle(fontSize: 24),),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+              ),
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, anim1, anim2) => const Page3(),
+                    transitionDuration: const Duration(seconds: 0),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatefulWidget {
+  const Page2({ Key? key }) : super(key: key);
+
+  @override
+  State<Page2> createState() => _Page2State();
+}
+
+class _Page2State extends State<Page2> {
+  @override
+  Widget build(BuildContext context) {
+    print('Page2');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page 2'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text('3페이지 교체', style: TextStyle(fontSize: 24),),
+              onPressed: (){
+                Navigator.pushReplacement(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, anim1, animation2) => const Page3(),
+                    transitionDuration: const Duration(seconds: 0),
+                  ),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: const Text('1페이지 교체', style: TextStyle(fontSize: 24),),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+              ),
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, anim1, animaion2) => const Page1(),
+                    transitionDuration: const Duration(seconds: 0),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Page3 extends StatefulWidget {
+  const Page3({ Key? key }) : super(key: key);
+
+  @override
+  State<Page3> createState() => _Page3State();
+}
+
+class _Page3State extends State<Page3> {
+  @override
+  Widget build(BuildContext context) {
+    print('Page3');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page 3'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text('1페이지 교체', style: TextStyle(fontSize: 24,)),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+              ),
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, anim1, aimation2) => const Page1(),
+                    transitionDuration: const Duration(seconds: 0),
+                  ),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: const Text('2페이지 교체', style: TextStyle(fontSize: 24),),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+              ),
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, anim1, animation2) => const Page2(),
+                    transitionDuration: const Duration(seconds: 0),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
