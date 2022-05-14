2fb6f7a8208d8f2bbcb75d08921b003002dd3265
diffth <diffth@nate.com>
2022-05-14 14:56:29 +0900

    Create 10.main-SharedPreferences.dart

---
+++ AppWidgets/10.main-SharedPreferences.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late int counter;
+  late SharedPreferences prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    getSharedPreferences();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 40,
+              child: FloatingActionButton(
+                heroTag: "minus",
+                child: Icon(Icons.remove),
+                onPressed: () {
+                  setState(() {
+                    counter--;
+                  });
+                  prefs.setInt("counter", counter);
+                },
+              ),
+            ),
+            Text("Shared preferences value: "),
+            Text(
+              "$counter",
+              style: TextStyle(fontSize: 30),
+            ),
+            SizedBox(
+              height: 40,
+              child: FloatingActionButton(
+                heroTag: "plus",
+                child: Icon(Icons.add),
+                onPressed: () {
+                  setState(() {
+                    counter++;
+                  });
+                  prefs.setInt("counter", counter);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  getSharedPreferences() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      counter = (prefs.getInt("counter")?? 0); 
+    });
+  }
+}
